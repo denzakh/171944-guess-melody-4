b051e8cf99054154fe7ebed0ed4999c012ec0382
denzakh <den.zakh@gmail.com>
2020-06-12 13:25:10 +0300

    правка расширений по умолчанию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
 import PropTypes from 'prop-types';
 
 const App = (props) => {
