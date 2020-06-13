e3dd9e99292d722cddc7b5157ae5fbfbcd19d735
denzakh <den.zakh@gmail.com>
2020-06-13 14:50:31 +0300

    правки для линтера на сервере

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -5,14 +5,14 @@ import WelcomeScreen from "./welcome-screen.jsx";
 
 Enzyme.configure({
   adapter: new Adapter
-})
+});
 
 describe(`App e2e`, ()=>{
   it(`should call handlerStart on start button click event`, ()=>{
     const onStartBtnClick = jest.fn();
 
     const welcomeScreen = shallow(
-      <WelcomeScreen time={1} errorCount={5} handlerStart={onStartBtnClick}  />
+      <WelcomeScreen time={1} errorCount={5} handlerStart={onStartBtnClick} />
     );
 
     const startBtn = welcomeScreen.find(`.welcome__button`);
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -7,12 +7,12 @@ let testData = {
   },
   time: 5,
   errorCount: 1
-}
+};
 
 describe(`WelcomeScreen component`, ()=>{
   it(`should render App component markup`, () => {
     const tree = renderer.create(
-        <WelcomeScreen  time={testData.time} errorCount={testData.errorCount} handlerStart={testData.handlerStart}  />
+      <WelcomeScreen time={testData.time} errorCount={testData.errorCount} handlerStart={testData.handlerStart} />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
